f7a8a222ef7f57b1b5740c443751ec185a02e9a0
victoriarickli <160596546+victoriarickli@users.noreply.github.com>
2024-04-09 08:04:30 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const botoes = document.querySelectorAll(".botao");
+ const botoes = document.querySelectorAll(".botao");
 for(let i=0; i<botoes.length; i++){
-    botoes[j].onclick = function(){
+    botoes [i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
-            botoes[i].classList.remove("ativo");
+            botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
     }
